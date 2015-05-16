7ac068d84c6c33548633e15a881ddf47ef66e847
Colby "CheeZe" Williams <colby@colbycheeze.com>
2015-05-16 17:53:05 -0500

    various style tweaks

--- client/helpers.js
+++
@@ -1,8 +0,0 @@
-Template.registerHelper("currentFieldValue", function (fieldName) {
-return AutoForm.getFieldValue("reactiveCurrentValueForm", fieldName) || "not selected";
-});
-
-Template.registerHelper("currentFieldValue2", function (fieldName) {
-return AutoForm.getFieldValue(fieldName) || "empty";
-});
-
--- server/fixtures.js
+++ server/fixtures.js
@@ -2,27 +2,27 @@ Meteor.startup(function () {
   if (!Polls.findOne()) {
     var userId, user;
 
-    if(user = Meteor.users.findOne({username: 'testUser'})) {
+    if(user = Meteor.users.findOne({usertitle: 'testUser'})) {
       userId = user._id;
     } else {
-      userId = Accounts.createUser({username: 'testUser', email: 'test@test.com', password: 'testtest'});
+      userId = Accounts.createUser({usertitle: 'testUser', email: 'test@test.com', password: 'testtest'});
     }
 
     Polls.insert({
       userId: userId,
-      name: 'Favorite ice cream',
+      title: 'Favorite ice cream',
       options: ['chocolate', 'vanilla', 'strawberry']
     });
 
     Polls.insert({
       userId: userId,
-      name: 'Favorite soda',
+      title: 'Favorite soda',
       options: ['coke', 'pepsi', 'sprite']
     });
 
     Polls.insert({
       userId: userId,
-      name: 'Favorite pokemon',
+      title: 'Favorite pokemon',
       options: ['pikachu', 'charmander', 'squirtle', 'bulbasaur']
     });
   }
