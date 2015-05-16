193ece04083b47036d289eb21db4ad09664b3970
Colby "CheeZe" Williams <colby@colbycheeze.com>
2015-05-16 10:22:20 -0500

    added nav

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,6 +1,13 @@
 FlowRouter.route('/', {
   name: 'home',
   action: function(params) {
-    FlowLayout.render('appLayout', { header: 'header', content: 'home', footer: 'footer' });
+    FlowLayout.render('appLayout', { header: 'header', content: 'Home', footer: 'footer' });
+  }
+});
+
+FlowRouter.route('/login', {
+  name: 'login',
+  action: function(params) {
+    FlowLayout.render('appLayout', { header: 'header', content: 'Login', footer: 'footer' });
   }
 });
