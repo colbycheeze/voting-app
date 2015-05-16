506eb0d0818cec5ed7e135273c22201395136666
lpatmo <linda.peng@alumni.duke.edu>
2015-05-16 14:49:55 -0400

    updated schema

--- collections/polls.js
+++ collections/polls.js
@@ -2,10 +2,14 @@ Polls = new Mongo.Collection("Polls");
 Schema = {};
 Schema.Polls = new SimpleSchema({
   title: {
+  	name: title,
     type: String,
+    max: 100
   },
   options: {
-    type: String
+  	name: options,
+    type: String,
+    max: 200
   }
 });
 
