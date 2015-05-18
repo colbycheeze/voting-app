ddaaf1c39893788f730206dc41fc7897bf001683
Colby "CheeZe" Williams <colby@colbycheeze.com>
2015-05-18 11:55:27 -0500

    logged-in users can vote on polls

--- client/views/polls/poll_view.js
+++ client/views/polls/poll_view.js
@@ -4,5 +4,20 @@ Template.Poll.helpers({
   },
   vote: function() {
     return Votes.findOne();
+  },
+  hasVoted: function() {
+    var pollId = Polls.findOne();
+    pollId = pollId ? pollId._id : undefined;
+
+    return Votes.find({userId: Meteor.userId(), pollId: pollId}).count() > 0;
+  }
+});
+
+Template.Poll.events({
+  'click [data-action="vote"]': function (e, tmpl) {
+    e.preventDefault();
+
+    var pollId = Polls.findOne()._id;
+    Votes.insert({option: this.valueOf(), userId: Meteor.userId(), pollId: pollId});
   }
 });
